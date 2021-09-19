ad67f42337f6f96e4c0692516b384e77806ecb28
Amethyst4966 <47138990+Chris4966@users.noreply.github.com>
2021-09-19 17:03:00 +0200

    Create PasswordGenerator.py

---
+++ PasswordGenerator.py
@@ -0,0 +1,32 @@
+import random
+
+def GenerateChar():
+    CharType = random.randint(1, 3)
+
+    UCLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    LCLetters = "abcdefghijklmnopqrstuvwxyz"
+    Numbers = "0123456789"
+
+    if CharType == 1:
+        index = random.randint(0, len(UCLetters)-1)
+        return UCLetters[index]
+    elif CharType == 2:
+        index = random.randint(0, len(LCLetters)-1)
+        return LCLetters[index]
+    else:
+        index = random.randint(0, len(Numbers)-1)
+        return Numbers[index]
+
+def WriteToFile(filename, password):
+    PWFile = open(filename, "a")
+    PWFile.write("Generated Password: " + password)
+    PWFile.close()
+
+PasswordLength = 10
+Password = []
+
+for i in range(PasswordLength):
+    Password.append(GenerateChar())
+
+print("".join(Password))
+WriteToFile("passwords.txt", "".join(Password))
